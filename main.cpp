31e116239754c0011e4649a09f9ed8f52e21ac43
Dimetil4ik <144273249+Dimetil4ik@users.noreply.github.com>
2023-09-07 08:26:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,18 +5,20 @@
 #include "histogram.h"
 #include "text.h"
 #include "svg.h"
+#include "Header.h"
 
-
-using namespace std;
+//using namespace std;
 
 struct Input {
-    vector<double> numbers;
+    std::vector<double> numbers;
     size_t bin_count{};
-    float n;
+    
 };
 
 Input
 input_data() {
+    using std::cerr;
+    using std::cin;
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
@@ -24,18 +26,12 @@ input_data() {
     Input in;
     in.numbers.resize(number_count);
     for (size_t i = 0; i < number_count; i++) {
-        cerr << "Enter number " << i+1 << ": ";
-        cin >> in.numbers[i];}
+        cerr << "Enter number " << i + 1 << ": ";
+        cin >> in.numbers[i];
+    }
     cerr << "Enter bin count: ";
     cin >> in.bin_count;
 
-    in.n = 0;
-    if (in.bin_count==0){
-        in.bin_count = sqrt(number_count);
-        in.n = 1;
-        if (in.bin_count > 25){
-            in.bin_count = 1 + log2(number_count);
-            in.n = 2;}}
 
     return in;
 }
