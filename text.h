5e8c7b381552824ddd8b039c7b065ca02d1f8a69
Dimetil4ik <144273249+Dimetil4ik@users.noreply.github.com>
2023-09-07 07:07:55 +0300

    Create text.h

---
+++ text.h
@@ -0,0 +1,8 @@
+#ifndef TEXT_H_INCLUDED
+#define TEXT_H_INCLUDED
+#include <vector>
+#include <iostream>
+
+void show_histogram_text(std::vector<size_t>& bins, size_t bin_count, float n);
+
+#endif // TEXT_H_INCLUDED
