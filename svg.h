f6b249d0b94858f0409b56fe88140dc952fff41d
Dimetil4ik <144273249+Dimetil4ik@users.noreply.github.com>
2023-09-07 07:06:46 +0300

    Create svg.h

---
+++ svg.h
@@ -0,0 +1,10 @@
+#ifndef SVG_H_INCLUDED
+#define SVG_H_INCLUDED
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+void show_histogram_svg(const std::vector<size_t>& bins);
+
+#endif // SVG_H_INCLUDED
